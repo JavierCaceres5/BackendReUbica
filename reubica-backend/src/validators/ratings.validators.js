9202215e9229189db358b64b3866961cd4418682
JavierCaceres5 <00068223@uca.edu.sv>
2025-06-27 19:45:35 -0600

    Ruta de obtener productos por emprendimiento y rutas de valoraciones de productos

--- reubica-backend/src/validators/ratings.validators.js
+++ reubica-backend/src/validators/ratings.validators.js
@@ -1,24 +1,10 @@
-import { body } from "express-validator";
-import { supabase } from "../config/config.js";
+import { param, body } from "express-validator";
 
 export const createRatingValidator = [
-  body("productoID")
+  param("productoID")
     .notEmpty()
-    .withMessage("productoID es requerido")
-    .bail()
     .isUUID()
-    .withMessage("productoID debe ser un UUID válido")
-    .bail()
-    .custom(async (productoID) => {
-      const { data, error } = await supabase
-        .from("Productos")
-        .select("id")
-        .eq("id", productoID)
-        .maybeSingle();
-      if (error) throw new Error("Error validando producto");
-      if (!data) throw new Error("El producto no existe");
-      return true;
-    }),
+    .withMessage("productoID inválido"),
 
   body("comentario")
     .optional()
@@ -27,17 +13,14 @@ export const createRatingValidator = [
 
   body("rating")
     .notEmpty()
-    .withMessage("rating es requerido")
-    .bail()
     .isFloat({ min: 1, max: 5 })
-    .withMessage("rating debe estar entre 1.0 y 5.0"),
+    .withMessage("rating debe estar entre 1 y 5")
 ];
 
 export const deleteRatingValidator = [
-  body("productoID")
+  param("productoID")
     .notEmpty()
-    .withMessage("productoID es requerido")
-    .bail()
     .isUUID()
-    .withMessage("productoID debe ser un UUID válido")
+    .withMessage("productoID es requerido o inválido")
 ];
+
