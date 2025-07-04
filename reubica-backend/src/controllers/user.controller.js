e1211ade8260b9a6d53a5e50ec631db3ebc4cb87
Marceloereyes <00148023@uca.edu.sv>
2025-07-04 09:00:02 -0600

    Actualizacion de user

--- reubica-backend/src/controllers/user.controller.js
+++ reubica-backend/src/controllers/user.controller.js
@@ -365,3 +365,20 @@ export async function resetPassword(req, res) {
     res.status(500).json({ error: err.message });
   }
 }
+
+export const getOwnUserProfileController = async (req, res) => {
+  try {
+    const userId = req.user.id;
+
+    const user = await usersService.getUserByIdService(userId);
+
+    if (!user) {
+      return res.status(404).json({ error: "Usuario no encontrado" });
+    }
+
+    res.status(200).json(user);
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
